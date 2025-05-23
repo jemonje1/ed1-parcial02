bf2986520fc38a9c66234c766eb82bc4ec61ca58
javu2 <jemonje@correo.url.edu.gt>
2025-05-23 08:19:54 -0600

    Pushes de testcases

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -1,11 +1,31 @@
 package ed.lab;
 
-import java.util.List;
+import java.util.*;
 
 public class E01MeetingRooms {
-
     public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
-        return -1;
+        int count = 0;
+        Map<Integer, MeetingInterval> map = new HashMap<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> a - b);
+        for (MeetingInterval m : meetingIntervals) {
+            map.put(m.endTime(), m);
+            pq.add(m.endTime());
+        }
+        List<MeetingInterval> activeMeetings = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            int end = pq.poll();
+            MeetingInterval current = map.get(end);
+            List<MeetingInterval> updatedActive = new ArrayList<>();
+            for (MeetingInterval m : activeMeetings) {
+                if (m.endTime() > current.startTime()) {
+                    updatedActive.add(m);
+                }
+            }
+            updatedActive.add(current);
+            count = Math.max(count, updatedActive.size());
+            activeMeetings = updatedActive;
+        }
+        return count;
     }
 
 }
--- src/main/java/ed/lab/E02Logger.java
+++ src/main/java/ed/lab/E02Logger.java
@@ -1,12 +1,20 @@
 package ed.lab;
 
+import java.util.HashMap;
+
 public class E02Logger {
-
+    public HashMap<String, Integer> map = new HashMap<>();
     public E02Logger() {
+        map = new HashMap<>();
     }
-
     public boolean shouldPrintMessage(int timestamp, String message) {
-        return false;
+        if (!map.containsKey(message) || timestamp >= map.get(message)) {
+            map.put(message, timestamp + 10);
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 }
